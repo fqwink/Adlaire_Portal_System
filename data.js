774287f788c9d8b2bfccbadea820f2fed82f846f
fqwink <fqwink@gmail.com>
2026-09-03 04:32:30 +0900

    初期コミット

---
+++ data.js
@@ -0,0 +1,54 @@
+// Adlaire Portal System - Configuration Data
+// Generated: 2026-02-14
+
+const PORTAL_CONFIG = {
+  "title": "Adlaireポータル",
+  "themeColor": "#00a968",
+  "news": [
+    {
+      "date": "2026/02/14",
+      "text": "🎉 Adlaireポータルシステムへようこそ!"
+    },
+    {
+      "date": "2026/02/10",
+      "text": "📢 編集画面から自由にカスタマイズできます"
+    }
+  ],
+  "categories": [
+    {
+      "name": "🔧 よく使うツール",
+      "links": [
+        {
+          "name": "Google",
+          "url": "https://google.com",
+          "icon": "🔍"
+        },
+        {
+          "name": "カレンダー",
+          "url": "https://calendar.google.com",
+          "icon": "📅"
+        },
+        {
+          "name": "Gmail",
+          "url": "https://mail.google.com",
+          "icon": "✉️"
+        }
+      ]
+    },
+    {
+      "name": "📂 ドキュメント",
+      "links": [
+        {
+          "name": "Google Drive",
+          "url": "https://drive.google.com",
+          "icon": "📂"
+        },
+        {
+          "name": "Dropbox",
+          "url": "https://dropbox.com",
+          "icon": "💼"
+        }
+      ]
+    }
+  ]
+};
